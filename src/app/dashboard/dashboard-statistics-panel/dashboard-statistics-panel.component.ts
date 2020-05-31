3cbbcbc9691de6080a817ce2d741840cf336bd6d
Nadoiv <nadoiv@gmail.com>
2020-05-31 13:22:40 +0300

    added statistics doughnut chart

--- src/app/dashboard/dashboard-statistics-panel/dashboard-statistics-panel.component.ts
+++ src/app/dashboard/dashboard-statistics-panel/dashboard-statistics-panel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../dashboard.service';
+import * as _ from "lodash";
+import Chart from 'chart.js';
 
 @Component({
   selector: 'dashboard-statistics-panel',
@@ -8,10 +10,50 @@ import { DashboardService } from '../dashboard.service';
 })
 export class DashboardStatisticsPanelComponent implements OnInit {
 
+  expenses;
+
   constructor(private dashboardService: DashboardService) { }
 
   ngOnInit(): void {
-    this.dashboardService.getExpensesForUser(JSON.parse(localStorage.getItem('currentUser')).id).subscribe(data => console.log(data));
+    const ctx = (document.getElementById('expensesChart') as HTMLCanvasElement).getContext('2d');
+
+    this.dashboardService.getExpensesForUser(JSON.parse(localStorage.getItem('currentUser')).id)
+      .subscribe(data => {
+        this.expenses = data;
+        const groupedByExpenses = this.groupExpenses(data);
+
+        this.createChart(ctx, groupedByExpenses);
+      });
   }
 
+  private groupExpenses(expenses) {
+    const formatedExpenses = expenses.map(expense => {
+      return {
+        expense: expense.expense,
+        category: expense.business.category.name
+      };
+    });
+
+    return _(formatedExpenses)
+      .groupBy('category')
+      .map((expense, category) => ({
+        category: category,
+        expense: _.sumBy(expense, 'expense')
+      })).value();
+  }
+
+  private createChart(ctx, groupedByExpenses) {
+    const data = {
+      datasets: [{
+        data: groupedByExpenses.map(expense => expense.expense),
+        backgroundColor: ['#083D77', '#F94144', '#e5989b', '#F8961E', '#F9C74F', '#90BE6D', '#43AA8B', '#577590']
+      }],
+      labels: groupedByExpenses.map(expense => expense.category)
+    }
+
+    new Chart(ctx, {
+      type: 'doughnut',
+      data: data
+    });
+  }
 }
